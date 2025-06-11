c4e62cda3dbadca00a7ca0db2f9cbd735be326d2
SergioCantillo-Cotel <sergio.cantillo@cotel.com.co>
2025-06-11 07:53:08 -0500

    Update tools.py

--- utils/tools.py
+++ utils/tools.py
@@ -6,6 +6,7 @@ from retry_requests import retry
 from google.oauth2 import service_account
 from pandas_gbq import read_gbq
 import pandas as pd
+import numpy as np
 
 credenciales_json = st.secrets["gcp_service_account"]
 BIGQUERY_PROJECT_ID = st.secrets["bigquery"]["project_id"]
@@ -28,22 +29,33 @@ def bigquery_auth():
 
 def read_bq_db(credentials):
     _POWER = ['PM_General_Potencia_Activa_Total', 'PM_Aires_Potencia_Activa_Total', 'Inversor_Solar_Potencia_Salida']
-    _INTERN = ['ocupacion_sede','IDU_0_1_Room_Temperature','IDU_0_2_Room_Temperature','IDU_0_1_Room_Temperature',
-               'IDU_0_3_Room_Temperature','IDU_0_4_Room_Temperature','IDU_0_5_Room_Temperature',
-               'IDU_0_6_Room_Temperature','IDU_0_7_Room_Temperature','IDU_0_8_Room_Temperature',
-               'IDU_0_9_Room_Temperature','IDU_0_10_Room_Temperature']
+    _TEMP = [f'IDU_0_{i}_Room_Temperature' for i in range(1, 11)]+[f'IDU_0_{i}_Estado_valvula' for i in range(1, 11)]+['ocupacion_sede']
+    _OCCUP = ['ocupacion_UMA_1','ocupacion_UMA_2','ocupacion_UMA_3','ocupacion_UMA_4',
+              'ocupacion_UMA_5','ocupacion_flotante'] 
     _MAPPING = {'PM_General_Potencia_Activa_Total': 'General','PM_Aires_Potencia_Activa_Total': 'Aires Acondicionados',
                 'Inversor_Solar_Potencia_Salida': 'SSFV','ocupacion_sede': 'Ocupacion',
-                **{f'IDU_0_{i}_Room_Temperature': f'T{i}' for i in range(1, 11)}}
-    df = read_gbq(f"SELECT * FROM `{TABLA_COMPLETA}` ORDER BY datetime_record ASC", project_id=BIGQUERY_PROJECT_ID, credentials=credentials).rename(columns={'id': 'unique_id', 'datetime_record': 'ds'})
+                'ocupacion_flotante':'Flotantes',
+                **{f'IDU_0_{i}_Room_Temperature': f'T{i}' for i in range(1, 11)},
+                **{f'IDU_0_{i}_Estado_valvula': f'Valvula_{i}' for i in range(1, 11)},
+                'ocupacion_UMA_1':'Z1: Sala de Juntas - Cubiculos',
+                'ocupacion_UMA_2':'Z2: Gerencia - Area TI',
+                'ocupacion_UMA_3':'Z3: G. Humana - EE - Preventa',
+                'ocupacion_UMA_4':'Z4: Contabilidad - Sala de Juntas',
+                'ocupacion_UMA_5':'Z5: G. Humana - Depto. Jurídico'}
+    df = read_gbq(f"SELECT * FROM `{TABLA_COMPLETA}` ORDER BY datetime_record ASC, id ASC", project_id=BIGQUERY_PROJECT_ID, credentials=credentials).rename(columns={'id': 'unique_id', 'datetime_record': 'ds'})
+    #df['ds'] = pd.to_datetime(df['ds']).dt.floor('15min')
     df_power = df[df['unique_id'].isin(_POWER)].copy()
     df_power['value'] *= 0.25
     df_power['ds'] = pd.to_datetime(df_power['ds']).dt.floor('15min')
     df_power['unique_id'] = df_power['unique_id'].map(_MAPPING)
     df_power = gen_others_load(df_power)
-    df_other = df[df['unique_id'].isin(_INTERN)].copy()
-    df_other['unique_id'] = df_other['unique_id'].map(_MAPPING)
-    return df_power, df_other
+    
+    df_AC = df[df['unique_id'].isin(_TEMP)].copy()
+    df_AC['unique_id'] = df_AC['unique_id'].map(_MAPPING)
+    
+    df_occup = df[df['unique_id'].isin(_OCCUP)]
+    df_occup['unique_id'] = df_occup['unique_id'].map(_MAPPING) 
+    return df_power, df_AC, df_occup
 
 def gen_others_load(df):
     pivot = (df.pivot_table(index=['ds', 'unit', 'company', 'headquarters'],columns='unique_id',values='value').reset_index())
@@ -55,15 +67,14 @@ def get_climate_data_1m(lat, lon):
     session = retry(requests_cache.CachedSession('.cache', expire_after=3600), retries=5, backoff_factor=0.2)
     r = openmeteo_requests.Client(session=session).weather_api("https://api.open-meteo.com/v1/forecast", params={
         "latitude": lat, "longitude": lon,
-        "hourly": ["apparent_temperature", "relative_humidity_2m", "precipitation"],
-        "start_date": "2025-05-15", "end_date": datetime.now().strftime("%Y-%m-%d")
-    })[0].Hourly()
+        "minutely_15": ["apparent_temperature", "relative_humidity_2m", "precipitation"],
+        "start_date": "2025-05-15", "end_date": datetime.now().strftime("%Y-%m-%d")})[0].Minutely15()
     idx = pd.date_range(start=pd.to_datetime(r.Time(), unit="s"), end=pd.to_datetime(r.TimeEnd(), unit="s"),
                         freq=pd.Timedelta(seconds=r.Interval()), inclusive="left")
     df = pd.DataFrame({"ds": idx, "T2M": r.Variables(0).ValuesAsNumpy(), "RH2M": r.Variables(1).ValuesAsNumpy(),
-                       "PRECTOTCORR": r.Variables(2).ValuesAsNumpy()}).set_index("ds").resample("15min").interpolate("linear").round(2)
+                       "PRECTOTCORR": r.Variables(2).ValuesAsNumpy()}).set_index("ds")
     df.index -= pd.Timedelta(hours=5)
-    return df.loc["2025-05-15 16:15:00":datetime.now() - pd.Timedelta(hours=5)].reset_index()
+    return df.loc["2025-05-15 16:15:00":datetime.now()].reset_index()
 
 # Función para obtener las métricas
 def get_metrics(general, ac, ssfv, otros):
@@ -79,6 +90,11 @@ def get_submedidores(metrics):
 def load_custom_css(file_path: str = "styles/style.css"):
     with open(file_path) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+def get_temp_prom(db1):
+    df_temp = db1.loc[db1["unit"] == '°C']
+    promedios = (df_temp.groupby(df_temp['ds'].dt.strftime('%Y-%m-%d %H:%M:%S'))['value'].mean())
+    return promedios.iloc[-1]
 
 def agenda_bms(ruta, fecha, num_personas, temp_ext, temp_int):
     df = pd.read_excel(ruta, usecols=[0, 1, 2, 3], names=['dia', 'hora', '_', 'intensidad'])
@@ -112,26 +128,33 @@ def agenda_bms(ruta, fecha, num_personas, temp_ext, temp_int):
                  4 if delta <= 5 else
                  6 if delta <= 10 else 7)
 
-    return (f"Hoy, {dia_S} a las {h}:{fecha.minute:02}, la programación Estándar del BMS indica que la Intensidad de Aires esté al {b}%.\n"
-            f"Ahora, dado que hay {num_personas:.0f} personas en la sede, temperaturas externa e interna de {temp_ext:.1f} °C y {temp_int:.1f} °C respectivamente, "
-            f"el modelo IA sugiere una intensidad de {p:.0f}% con una velocidad de ventiladores de {categoria}"), p, b, categoria
+    return dia_S, p
 
-def seleccionar_unidades(pred, intensidad_base):
-    tabla_intensidad = {
-        0:  [0, 0, 0, 0, 0],
-        15: [1, 0, 0, 0, 0],
-        25: [1, 0, 1, 0, 0],
-        50: [1, 0, 1, 1, 0],
-        75: [1, 0, 1, 1, 1],
-        100:[1, 1, 1, 1, 1],
-    }
 
-    diferencia = abs(pred - intensidad_base)
-    intensidades = sorted(tabla_intensidad.keys())
-
-    if diferencia < 10:
-        intensidad_cercana = max([i for i in intensidades if i <= intensidad_base], default=0)
+def seleccionar_unidades(pred,personas,fecha,dia):
+    zonas = personas[(personas["ds"] == personas["ds"].max()) & (personas["unique_id"] != 'Flotantes')]
+    aires = round(pred / 20,0)
+    base = np.array([0, 20, 40, 60, 80, 100])
+    zonas['proporcion_ocup'] = zonas['value']/zonas['value'].sum()
+    zonas = zonas.sort_values(by='proporcion_ocup', ascending=False)
+    zonas['encendido'] = 0
+    zonas.loc[zonas.head(int(aires)).index, 'encendido'] = 1
+    zonas = zonas.sort_values(by='unique_id', ascending=True)
+    diferencia = pred - base[base < pred]
+    mas = diferencia[-1] if len(diferencia) > 0 else 0
+    velocidad_valor = round(mas / 20 * 7)
+    zonas['velocidad_ventilador'] = zonas['encendido'] * velocidad_valor
+    if zonas['encendido'].sum()==0:
+        mensaje = (f"Cotel IA sugiere en este momento, {dia} a las {fecha.hour}:{fecha.strftime('%M')}," 
+        " que los aires acondiconados en las distintas zonas estén apagados de acuerdo con las condiciones de control.")
+        encendidas = [0,0,0,0,0]
     else:
-        intensidad_cercana = max([i for i in intensidades if i <= pred], default=0)
-
-    return tabla_intensidad[intensidad_cercana]
+        encendidas = zonas['encendido'].values.tolist()
+        mensaje = (
+            f"Cotel IA sugiere en este momento, {dia} a las {fecha.hour}:{fecha.strftime('%M')}, "
+            "que sean encendidos los aires en las siguientes zonas de acuerdo con las condiciones de control: "
+            f"{', '.join(zonas[zonas['encendido'] == 1]['unique_id'].to_list())}.\n\n"
+            "Invitamos a las personas que se encuentren en otros espacios y quieran disfrutar de un mayor confort "
+            "a ubicarse en estos lugares y disfruten de la compañía de la familia Cotel."
+        )
+    return encendidas, zonas['velocidad_ventilador'].values.tolist(), mensaje
