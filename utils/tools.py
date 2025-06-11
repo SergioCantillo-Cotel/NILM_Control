7b7ea6a44665ac96264eae64489b80cec76869aa
SergioCantillo-Cotel <sergio.cantillo@cotel.com.co>
2025-06-11 08:47:24 -0500

    Update tools.py

--- utils/tools.py
+++ utils/tools.py
@@ -68,7 +68,7 @@ def get_climate_data_1m(lat, lon):
     r = openmeteo_requests.Client(session=session).weather_api("https://api.open-meteo.com/v1/forecast", params={
         "latitude": lat, "longitude": lon,
         "minutely_15": ["apparent_temperature", "relative_humidity_2m", "precipitation"],
-        "start_date": "2025-05-15", "end_date": datetime.now().strftime("%Y-%m-%d")})[0].Minutely15()
+        "start_date": "2025-05-15", "end_date": (datetime.now() - - timedelta(hours=5)).strftime("%Y-%m-%d")})[0].Minutely15()
     idx = pd.date_range(start=pd.to_datetime(r.Time(), unit="s"), end=pd.to_datetime(r.TimeEnd(), unit="s"),
                         freq=pd.Timedelta(seconds=r.Interval()), inclusive="left")
     df = pd.DataFrame({"ds": idx, "T2M": r.Variables(0).ValuesAsNumpy(), "RH2M": r.Variables(1).ValuesAsNumpy(),
