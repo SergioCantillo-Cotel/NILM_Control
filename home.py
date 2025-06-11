4360d84d072f9119e91fc56c22876e98e1e4c9fd
SergioCantillo-Cotel <sergio.cantillo@cotel.com.co>
2025-06-11 07:51:47 -0500

    Update home.py

--- home.py
+++ home.py
@@ -4,15 +4,18 @@ from utils import tools
 import pandas as pd
 import pytz
 
-st.set_page_config(page_title="Home", layout="wide")
+st.set_page_config(page_title="Eficiencia Energética + IA Cotel", layout="wide")
 tools.load_custom_css()
 st.logo("images/cotel-logotipo.png", size="Large")
-NILM = st.Page("pages/Pagina1.py", title="Medición Inteligente no Intrusiva", icon="⚡",)
-SBCon = st.Page("pages/Pagina2.py", title="Smart Building Control", icon="🏢")
 
-pg = st.navigation([NILM, SBCon])
+NILM = st.Page("pages/NILM.py", title="Cotel - Sede la Flora", icon="🏛️",)
+SBMon = st.Page("pages/SBC_Mon.py", title="Monitoreo", icon="📈")
+SBCon = st.Page("pages/SBC_Cont.py", title="Climatización", icon="❄️")
+pg = st.navigation({"⚡ Medición Inteligente no Intrusiva": [NILM],
+                    "🏢 Smart Building Control": [SBMon,SBCon]})
 pg.run()
 
 zona = pytz.timezone("America/Bogota")
 ahora = pd.Timestamp(datetime.now(zona)).floor('15min').strftime("%Y-%m-%d %H:%M:%S")
-st.caption(f"🔄 Esta página se actualiza cada 15 minutos. Última actualización: {ahora}")
+st.markdown(f"""<div class="footer">🔄 Esta página se actualiza cada 15 minutos. Última actualización: {ahora}</div>
+""", unsafe_allow_html=True)
