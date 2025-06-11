bb232475a867dc100c2a1abc47adc3a5aad92816
SergioCantillo-Cotel <sergio.cantillo@cotel.com.co>
2025-06-11 08:46:08 -0500

    Update tools.py

--- utils/tools.py
+++ utils/tools.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit_autorefresh import st_autorefresh
-from datetime import datetime
+from datetime import datetime, timedelta
 import requests_cache, holidays, openmeteo_requests
 from retry_requests import retry
 from google.oauth2 import service_account
