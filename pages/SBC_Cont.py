a9abb90d614baec00b0c26d12c5b3c0e2c49ffc8
SergioCantillo-Cotel <sergio.cantillo@cotel.com.co>
2025-06-13 07:19:03 -0500

    Update SBC_Cont.py

--- pages/SBC_Cont.py
+++ pages/SBC_Cont.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from utils import tools, viz
 
-tools.quarter_autorefresh()
+tools.quarter_autorefresh(key='a')
 credentials = tools.bigquery_auth()
 db_pow, db_temp, db_occup = tools.read_bq_db(credentials)
 lat, lon = 3.4793949016367822, -76.52284557701176
